0bbc9f2ccec92f0a1395e1a61f3822664afc4bd8
Sam Agnew <sagnew@eden.rutgers.edu>
2017-07-19 14:25:21 -0700

    Add functionality to download all tracks by a given artist

--- soundcloud_downloader.py
+++ soundcloud_downloader.py
@@ -5,20 +5,51 @@ import requests
 import soundcloud
 
 
+soundcloud_client = soundcloud.Client(client_id=os.environ['SOUNDCLOUD_ID'])
+
+
 def download_track(track_url):
-    soundcloud_client = soundcloud.Client(client_id=os.environ['SOUNDCLOUD_ID'])
-    resolved_data = soundcloud_client.get('/resolve', url=track_url, allow_redirects=False)
+    resolved_data = soundcloud_client.get('/resolve',
+                                          url=track_url, allow_redirects=False)
 
     track_data = soundcloud_client.get(resolved_data.url)
     track_title = track_data.obj['title']
     track_id = track_data.obj['id']
 
     soundcloud_stream_resource = track_data.obj['stream_url']
-    stream_url = soundcloud_client.get(soundcloud_stream_resource, allow_redirects=False)
+    stream_url = soundcloud_client.get(soundcloud_stream_resource,
+                                       allow_redirects=False)
 
-    with open('{} - {}.mp3'.format(track_title, track_id), 'wb') as f:
+    # Create a legal filename.
+    filename = "".join([c for c in track_title
+                              if c.isalpha() or c.isdigit() or c==' ']).rstrip()
+
+    with open('{} - {}.mp3'.format(filename, track_id), 'wb') as f:
         f.write(requests.get(stream_url.location).content)
 
+
+def download_artist(username):
+    artist_url = 'http://soundcloud.com/{}'.format(username)
+    resolved_data = soundcloud_client.get('/resolve',
+                                          url=artist_url,
+                                          allow_redirects=False)
+
+    user_resource = soundcloud_client.get(resolved_data.obj['location'])
+    user_tracks_resource = '/users/{}/tracks'.format(user_resource.obj['id'])
+    user_tracks = soundcloud_client.get(user_tracks_resource)
+
+    for track in user_tracks:
+        print('Downloading: {}'.format(track.obj['title']))
+
+        try:
+            download_track(track.obj['permalink_url'])
+        except:
+            print('Problem downloading {}'.format(track.obj['title']))
+
+
 if __name__ == '__main__':
-    print(sys.argv[1])
-    download_track(sys.argv[1])
+    if sys.argv[1] == 'track':
+        download_track(sys.argv[1])
+    elif sys.argv[1] == 'artist':
+        download_artist(sys.argv[2])
+
